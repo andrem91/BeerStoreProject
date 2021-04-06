37dbf176f4c9e298ee9927f563680db8e8081d6f
andrem91 <andrem166@gmail.com>
2021-04-06 20:46:25 -0300

    Ajustes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,17 +1,17 @@
 import { Switch, Route } from 'react-router-dom'
 
-import Home from './Pages/Home'
-import Lojas from './Pages/Lojas'
-import PageProdutos from './Pages/Produtos'
-import Depoimentos from './Pages/Depoimentos'
+import Home from './pages/Home'
+import Lojas from './pages/Lojas'
+import PageProdutos from './pages/Produtos'
+import Depoimentos from './pages/Depoimentos';
 
 function Routes() {
     return (
         <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/produtos" component={ PageProdutos } />
-            <Route exact path="/lojas" component={ Lojas } />
-            <Route exact path="/depoimentos" component={ Depoimentos } />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/produtos" component={PageProdutos} />
+            <Route exact path="/lojas" component={Lojas} />
+            <Route exact path="/depoimentos" component={Depoimentos} />
         </Switch>
     )
 }
